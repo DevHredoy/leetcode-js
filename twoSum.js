280c46158bae5af11fb914344fec01a630221fae
DevHredoy <hredoy.rafiul@gmail.com>
2025-01-22 20:50:22 +0600

    initial commit

---
+++ twoSum.js
@@ -0,0 +1,17 @@
+// sorting the array
+// seting point in two edges , setting a middle point.
+//if the sum of i+j is bigger it shall come left from the higher boundary.
+//if sum is little than the target it shall come to one right direction from i
+
+
+const array=[23,88,8,35,14,53,67,98,80,32];
+array.sort(function(a,b){return a-b});
+
+//if the sum becomes greater 
+while()
+console.log("array:",array)
+
+
+
+
+
