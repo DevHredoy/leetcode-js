ac1cd8a76e6ca179510c0ef5235703e848f51cb3
DevHredoy <rafiul.hredoy@technonext.com>
2025-02-05 11:35:48 +0600

    todo: Roman numbe convertion

---
+++ romanNumberConv.js
@@ -0,0 +1,19 @@
+
+/*
+
+numbers to be converted to roman number
+
+I             1
+V             5
+X             10
+
+
+
+29  xxix
+
+if it is divisible by 10. the number of x will be  nX=nR/10
+
+the rest number which will be added nR%10
+
+todo:lets implement this for untill number 30
+*/
